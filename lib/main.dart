ccf9c6271d5238abcf577b6976754f41612e0e27
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-12-31 08:29:27 +0700

    feat(main): initialize FCM and local notification services

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:test_background_service/app/app.dart';
 import 'package:test_background_service/firebase_options.dart';
+import 'package:test_background_service/firebase_message_services.dart';
+import 'package:test_background_service/notifications_services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // TODO -> Initialize Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // TODO -> Service Notification Messaging
+  await NotificationService.instance.init();
+  await FirebaseMessagingService.instance.init();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
