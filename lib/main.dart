6175d52830120ea5cc1c366df48d9d75b7cf1801
reowicaksono <reo.wicaksono.2005356@gmail.com>
2026-01-01 20:59:33 +0700

    chore(env): env init

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:test_background_service/app/app.dart';
 import 'package:test_background_service/firebase_options.dart';
 import 'package:test_background_service/firebase_message_services.dart';
@@ -17,6 +18,9 @@ void main() async {
   await NotificationService.instance.init();
   await FirebaseMessagingService.instance.init();
   FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+  // TODO -> Initialize Environment
+  await dotenv.load(fileName: ".env");
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
