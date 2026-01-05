d3ab2cf6684fb2b77635c7792c217ce14ffbb2a3
reowicaksono <reo.wicaksono.2005356@gmail.com>
2026-01-05 10:24:09 +0700

    chore(logging): add optional data with formatted message

--- lib/app/logger/app_logger.dart
+++ lib/app/logger/app_logger.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:developer' as dev;
 import 'package:test_background_service/app/logger/enum_logger.dart';
 
@@ -10,17 +11,32 @@ class AppLogger {
     String message, {
     LogLevel level = LogLevel.debug,
     Object? error,
+    Object? data,
     StackTrace? stackTrace,
     String tag = 'App',
   }) {
     if (!enable) return;
+    final formatedMessage = data != null
+        ? '$message | data: ${_stringifyData(data)}'
+        : message;
     dev.log(
-      message,
+      formatedMessage,
       name: tag,
       level: _mapLevel(level),
       error: error,
       stackTrace: stackTrace,
     );
+  }
+
+  static String _stringifyData(Object data) {
+    try {
+      if (data is Map || data is List) {
+        return jsonEncode(data);
+      }
+      return data.toString();
+    } catch (_) {
+      return data.toString();
+    }
   }
 
   static int _mapLevel(LogLevel level) {
